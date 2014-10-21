061f24f6444ce1f87d06c302f4328e4749fc41a2
Brian Okoro <bokoro@caltech.edu>
2014-10-20 23:22:37 -0700

    shit looks nice

--- sc_pagerank.py
+++ sc_pagerank.py
@@ -11,92 +11,108 @@ def getNeighbors(artist, artistGraph):
 	# The following three lists are gathered to compute the outNeighbors list.
 	# A soundcloud user is considered an outNeighbor if they are followed by,
 	# have a track favorited by, or a track commented on by the given artist.
-	try:
-
-		# get list of users who the artist is following.
-		# consider we may not want to always analyze the first 100, although it works since it should be
-		# the hundred most frequent 
-		followings = client.get('/users/' + str(artist) + '/followings', limit=100)
-		print "Analyzing " + str(artist) + "\'s " + str(len(followings)) + " followings..."
-		for user in followings:
-			if not artistGraph.__contains__(user.id):
-				try:
-					artistGraph.add_node(user.id)
-					artistGraph.node[user.id]['currPR'] = 0
-					artistGraph.node[user.id]['newPR'] = 0
-					artistGraph.node[user.id]['marked'] = 0
-					artistGraph.add_edge(artist, user.id, key = fol_weight, weight = 1)
-				except:
-					print "Unexpected error:", sys.exc_info()[0]	
-
-		# get list of songs the artist favorites.
-		favorites = client.get('/users/' + str(artist) + '/favorites', limit=100)
-		print "Analyzing " + str(artist) + "\'s " + str(len(favorites)) + " favorites..."
-		for user in favorites:
-			if artistGraph.has_edge(artist, user.id, fol_weight):
-				if artistGraph.has_edge(artist, user.id, fav_weight):
-					artistGraph.add_edge(artist, user.id, key = fav_weight, 
-						weight = artistGraph.get_edge_data(artist, user.id, key = fav_weight) + 1)
-				else:
-					artistGraph.add_edge(artist, user.id, key = fav_weight, weight = 1)	
-
-		# get list comments the artist has made.
-		comments = client.get('/users/' + str(artist) + '/comments', limit=100)
-		print "Analyzing " + str(artist)  + "\'s " + str(len(comments)) + " comments..."
-		for user in comments:
-			if artistGraph.has_edge(artist, user.id, fol_weight):
-				if artistGraph.has_edge(artist, user.id, com_weight):
-					artistGraph.add_edge(artist, user.id, key = com_weight, 
-						weight = artistGraph.get_edge_data(artist, user.id, key = com_weight) + 1)
-				else:
-					artistGraph.add_edge(artist, user.id, key = com_weight, weight = 1)	
-		# The following two lists are gathered to compute the inNeighbors list.
-		# A soundcloud user is considered an inNeighbor if they follow,
-		# favorite, or have commented on a track by the given artist.
-
-		# get list of who follows the artist
-		followers = client.get('/users/' + str(artist) + '/followers', limit=100)
-		print "Analyzing " + str(artist) + "\'s " + str(len(followers)) + " followers..."
-		for user in followers:
-			if not artistGraph.__contains__(user.id):
-				try:
-					artistGraph.add_node(user.id)
-					artistGraph.node[user.id]['currPR'] = 0
-					artistGraph.node[user.id]['newPR'] = 0
-					artistGraph.node[user.id]['marked'] = 0
-					artistGraph.add_edge(user.id, artist, key = fol_weight, weight = 1)
-				except:
-					print "Unexpected error:", sys.exc_info()[0]
-
-		# get a list of tracks by the user in order to list the users
-		# having favorited, commented these tracks
-		artist_tracks = client.get('/users/' + str(artist) + '/tracks', limit=100)
-		print "Analyzing " + str(artist) + "\'s " + str(len(artist_tracks)) + " tracks..."
-		for track in artist_tracks:
-			try:
-			# get list of users who have favorited this users track 
-		 		favoriters = client.get('/tracks/' + str(track.id) + '/favoriters')
-		 		if artistGraph.has_edge(user.id, artist, fol_weight):
-					if artistGraph.has_edge(user.id, artist, fav_weight):
-						artistGraph.add_edge(user.id, artist, key = fav_weight, 
-							weight = artistGraph.get_edge_data(artist, user.id, key = fav_weight) + 1)
-				else:
-					artistGraph.add_edge(user.id, artist, key = fav_weight, weight = 1)
-
-				track_comments = client.get('/tracks/' + str(track.id) + '/comments')
-				if artistGraph.has_edge(user.id, artist.id, fol_weight):
-					if artistGraph.has_edge(user.id, artist.id, com_weight):
-						artistGraph.add_edge(user.id, artist, key = com_weight, 
-							weight = artistGraph.get_edge_data(artist, user.id, key = com_weight) + 1)
-				else:
-					artistGraph.add_edge(user.id, artist, key = com_weight, weight = 1)
-			except:
-				print "Unexpected error:", sys.exc_info()[0]
-		artistGraph.node[artist]['marked'] = 1			
 	
-	except:
-		print "Unexpected error:", sys.exc_info()[0]
+	# get list of users who the artist is following.
+	# consider we may not want to always analyze the first 100, although it works since it should be
+	# the hundred most frequent 
+	followings = client.get('/users/' + str(artist) + '/followings', limit=100)
+	a_dat = client.get('/users/' + str(artist))
 
+	try:
+		print "Analyzing " + str(a_dat.username) + "\'s " + str(len(followings)) + " followings..."
+	except:
+		print "Unicode Error, using artist ID: " + str(artist)	
+		
+	for user in followings:
+		if not artistGraph.__contains__(user.id):
+			artistGraph.add_node(user.id)
+			artistGraph.node[user.id]['currPR'] = 0
+			artistGraph.node[user.id]['newPR'] = 0
+			artistGraph.node[user.id]['marked'] = 0
+			artistGraph.add_edge(artist, user.id, key = 'fol_weight', weight = 1)
+			print "User successfully added to artistGraph!"
+			
+	# get list of songs the artist favorites.
+	favorites = client.get('/users/' + str(artist) + '/favorites', limit=100)
+
+	try:
+		print "Analyzing " + str(a_dat.username) + "\'s " + str(len(favorites)) + " favorites..."
+	except:
+		print "Unicode Error, using artist ID: " + str(artist)	
+
+	for user in favorites:
+		if artistGraph.has_edge(artist, user.id, 'fol_weight'):
+			if artistGraph.has_edge(artist, user.id, 'fav_weight'):
+				artistGraph.add_edge(artist, user.id, key = 'fav_weight', 
+					weight = artistGraph.get_edge_data(artist, user.id, key = 'fav_weight') + 1)
+			else:
+				artistGraph.add_edge(artist, user.id, key = 'fav_weight', weight = 1)	
+
+	# get list comments the artist has made.
+	comments = client.get('/users/' + str(artist) + '/comments', limit=100)
+
+	try:
+		print "Analyzing " + str(a_dat.username)  + "\'s " + str(len(comments)) + " comments..."
+	except:
+		print "Unicode Error, using artist ID: " + str(artist)	
+
+	for user in comments:
+		if artistGraph.has_edge(artist, user.id, 'fol_weight'):
+			if artistGraph.has_edge(artist, user.id, 'com_weight'):
+				artistGraph.add_edge(artist, user.id, key = 'com_weight', 
+					weight = artistGraph.get_edge_data(artist, user.id, key = 'com_weight') + 1)
+			else:
+				artistGraph.add_edge(artist, user.id, key = 'com_weight', weight = 1)	
+	# The following two lists are gathered to compute the inNeighbors list.
+	# A soundcloud user is considered an inNeighbor if they follow,
+	# favorite, or have commented on a track by the given artist.
+
+	# get list of who follows the artist
+	followers = client.get('/users/' + str(artist) + '/followers', limit=100)
+
+	try:
+		print "Analyzing " + str(a_dat.username) + "\'s " + str(len(followers)) + " followers..."
+	except:
+		print "Unicode Error, using artist ID: " + str(artist)	
+
+	for user in followers:
+		if not artistGraph.__contains__(user.id):
+			artistGraph.add_node(user.id)
+			artistGraph.node[user.id]['currPR'] = 0
+			artistGraph.node[user.id]['newPR'] = 0
+			artistGraph.node[user.id]['marked'] = 0
+			artistGraph.add_edge(user.id, artist, key = 'fol_weight', weight = 1)
+			print "User successfully added to artistGraph!"
+				
+	# get a list of tracks by the user in order to list the users
+	# having favorited, commented these tracks
+	artist_tracks = client.get('/users/' + str(artist) + '/tracks', limit=100)
+
+	try:
+		print "Analyzing " + str(a_dat.username) + "\'s " + str(len(artist_tracks)) + " tracks..."
+	except:
+		print "Unicode Error, using artist ID: " + str(artist)
+
+	for track in artist_tracks:
+		# get list of users who have favorited this users track 
+	 	favoriters = client.get('/tracks/' + str(track.id) + '/favoriters')
+	 	if artistGraph.has_edge(user.id, artist, 'fol_weight'):
+			if artistGraph.has_edge(user.id, artist, 'fav_weight'):
+				artistGraph.add_edge(user.id, artist, key = 'fav_weight', 
+					weight = artistGraph.get_edge_data(artist, user.id, key = 'fav_weight') + 1)
+		else:
+			artistGraph.add_edge(user.id, artist, key = 'fav_weight', weight = 1)
+
+		track_comments = client.get('/tracks/' + str(track.id) + '/comments')
+		if artistGraph.has_edge(user.id, artist, 'fol_weight'):
+			if artistGraph.has_edge(user.id, artist, 'com_weight'):
+				artistGraph.add_edge(user.id, artist, key = 'com_weight', 
+					weight = artistGraph.get_edge_data(artist, user.id, key = 'com_weight') + 1)
+		else:
+			artistGraph.add_edge(user.id, artist, key = 'com_weight', weight = 1)
+			
+	artistGraph.node[artist]['marked'] = 1			
+	
 def initializePR(artistGraph):
 	""" Sets the initial PR value of every artist in the dictionary to 1/num_artists."""
 	artists = artistGraph.nodes()
