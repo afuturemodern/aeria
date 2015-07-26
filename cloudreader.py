eb29947f9247e689d43d723b45b00719a6aac0a7
Jungle T <prometheusgalt@gmail.com>
2015-07-26 19:07:50 +0000

    610k+ profiles

--- cloudreader.py
+++ cloudreader.py
@@ -1,3 +1,20 @@
+import sys
+import networkx as nx
+import soundcloud
+import sc_api_calls as scac
+
+artistGraph = nx.MultiDiGraph()
+
+
+try:
+    print "Reading in artist graph..."
+    artistGraph = nx.read_pajek('artistGraph.net')
+    print "Read successfully!"
+    print "The artist graph currently contains " + str(len(artistGraph)) + " artists."
+    print "The artist graph currently contains " + str(nx.number_strongly_connected_components(artistGraph)) + " strongly connected components."
+except IOError:
+    print "Could not find artistGraph"
+
 for artist in artistGraph.nodes():
 	if artist:
 		try:
