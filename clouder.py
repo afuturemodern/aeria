07081ec49c246b738e658666d9f3af901b4338ce
Brian Okoro <chibuezebrokoro@gmail.com>
2015-09-07 01:40:06 +0000

    Clouder test uses methods from Cloudreader

--- clouder.py
+++ clouder.py
@@ -1,23 +1,19 @@
 import sys
 import networkx as nx
 import soundcloud
+
 from geoff import get_geoff
 from py2neo.ext.geoff import GeoffLoader
-artistGraph = nx.MultiDiGraph()
+
+from cloudreader import read_graph
+
 
 def post_to_cloud(G):
     GeoffLoader.load(get_geoff(G))
 
-try:
-    print "Reading artist graph..."
-    artistGraph = nx.read_pajek('artistGraph.net')
-    print "Read successfully!"
-    print "The artist graph currently contains " + str(len(artistGraph)) + " artists."
-    print "The artist graph currently contains " + str(nx.number_strongly_connected_components(artistGraph)) + " strongly connected components."
-except IOError:
-    print "Could not find artist graph."
-
-post_to_cloud(artistGraph)
+# artistGraph = nx.MultiDiGraph()
+# read_graph(artistGraph, 'artistGraph.net')
+# post_to_cloud(artistGraph)
     
 
 
