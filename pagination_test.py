1f3a111877d1cf5c78ab276751c7bb5189e4ed94
Giordon Stark <kratsg@gmail.com>
2016-02-07 11:35:42 -0600

    better return with generator expression

--- pagination_test.py
+++ pagination_test.py
@@ -1,25 +1,27 @@
 import soundcloud
 
+# let's return tracks via a generator instead...
+def get_tracks(client, page_size=100):
+    def get_results(next_href=None):
+        if next_href is None: return None
+        else: return client.get(next_href)
+
+    def yield_results():
+        # start results
+        results = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
+        while results is not None:
+            for result in getattr(results, 'collection', []):
+                yield result
+            results = get_results(getattr(results, 'next_href', None))
+
+    return yield_results()
+
 # create a client object with your app credentials
 client = soundcloud.Client(client_id='454aeaee30d3533d6d8f448556b50f23')
 
-page_size = 100
-
-results = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
 i = 0
-page = 0
-while True:
-    for track in results.collection:
-        print "\t", i, track.title
-        i += 1
+for track in get_tracks(client):
+    print i, track.title
+    i += 1
+    if i == 100: break
 
-    print "Finished page {0:d}".format(page)
-
-    if getattr(results, 'next_href', None) is None:
-        break
-
-    page += 1
-    # start paging through results, 100 at a time
-    results = client.get(results.next_href)
-
-print "Finished!"
