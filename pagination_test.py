f62cc2a26cc98d2006213a746f5bed2f1122c992
Giordon Stark <kratsg@gmail.com>
2016-02-07 10:59:09 -0600

    pagination works

--- pagination_test.py
+++ pagination_test.py
@@ -5,20 +5,21 @@ client = soundcloud.Client(client_id='454aeaee30d3533d6d8f448556b50f23')
 
 page_size = 100
 
-# get first 100 tracks
-results = client.get('/tracks', order='created_at', limit=page_size)
-
+results = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
 i = 0
 page = 0
 while True:
-    tracks = getattr(results, 'collection', results)
-    for track in tracks:
+    for track in results.collection:
         print "\t", i, track.title
         i += 1
 
     print "Finished page {0:d}".format(page)
 
+    if getattr(results, 'next_href', None) is None:
+        break
+
     page += 1
     # start paging through results, 100 at a time
-    results = client.get('/tracks', order='created_at', limit=page_size,
-                        linked_partitioning=page)
+    results = client.get(results.next_href)
+
+print "Finished!"
