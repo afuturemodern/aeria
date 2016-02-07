0603f48a2242dd564e7a9413a672d76ad427a970
Giordon Stark <kratsg@gmail.com>
2016-02-07 11:44:01 -0600

    more generic, demonstrate how to do users and tracks with the same function

--- pagination_test.py
+++ pagination_test.py
@@ -1,18 +1,21 @@
 import soundcloud
 
 # let's return tracks via a generator instead...
-def get_tracks(client, page_size=100):
-    def get_results(next_href=None):
+def get_results(client, url, page_size=100):
+    def get_next_results(results=None):
+        if results is None:
+            return client.get(url, order='created_at', limit=page_size, linked_partitioning=1)
+        next_href = getattr(results, 'next_href', None)
         if next_href is None: return None
         else: return client.get(next_href)
 
     def yield_results():
         # start results
-        results = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
+        results = get_next_results(None)
         while results is not None:
             for result in getattr(results, 'collection', []):
                 yield result
-            results = get_results(getattr(results, 'next_href', None))
+            results = get_next_results(results)
 
     return yield_results()
 
@@ -20,8 +23,13 @@ def get_tracks(client, page_size=100):
 client = soundcloud.Client(client_id='454aeaee30d3533d6d8f448556b50f23')
 
 i = 0
-for track in get_tracks(client):
+for track in get_results(client, '/tracks'):
     print i, track.title
     i += 1
     if i == 100: break
 
+i = 0
+for user in get_results(client, '/users'):
+    print i, user.first_name, user.last_name
+    i+= 1
+    if i == 100: break
