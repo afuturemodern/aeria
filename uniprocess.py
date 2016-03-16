21f8f9a1218b86a350231d2748dc27d764ec58cd
Brian Okoro <chibuezebrokoro@gmail.com>
2016-03-16 15:25:46 +0000

    Uncaught error: Unknown format code 's' for object of type 'int'

--- uniprocess.py
+++ uniprocess.py
@@ -6,14 +6,14 @@ import sc_api_calls as scac
 
 from cloudprinter import print_graph
 
-# A global artist graph used to iterate through the various algorithms.
-# Each node is artist id, with edges weighted by activity between then.
-artistGraph = nx.MultiDiGraph()
+# A global profile graph used to iterate through the various algorithms.
+# Each node is profile id, with edges weighted by activity between then.
+profileGraph = nx.MultiDiGraph()
 
-# read_graph(artistGraph, 'artistGraph.net')
+# read_graph(profileGraph, 'profileGraph.net')
 client = soundcloud.Client(client_id='454aeaee30d3533d6d8f448556b50f23')
 
-raw_name = raw_input("Enter a soundcloud artist to analyze: ")
+raw_name = raw_input("Enter a soundcloud profile to analyze: ")
 
 # Artist of interest
 search = client.get('/users/', q = raw_name)[0]
@@ -24,79 +24,79 @@ print "Artist interpreted as: %s" % search.username
 print "="*20
 
 # initialize the task queue
-artists_to_enqueue = [search.id]
+profiles_to_enqueue = [search.id]
 
 depth = 2
 i = 0
 
-# list of artists we could not query
-unavailable_artists = []
+# list of profiles we could not query
+unavailable_profiles = []
 
 for t in range(depth):
 
 	print "Iteration " + str(t)
 
-	artists_to_enqueue = list(set(artists_to_enqueue))
+	profiles_to_enqueue = list(set(profiles_to_enqueue))
 
-	for artist in artists_to_enqueue:
-		username = scac.id2username(artist)
+	for profile in profiles_to_enqueue:
+		username = scac.id2username(profile)
 		if username:
-			print "\t", "Enqueueing: %s (%s)" % (username, artist)
-                        artistGraph.add_node(artist)
+			print "\t", "Enqueueing: %s (%s)" % (username, profile)
+                        profileGraph.add_node(profile)
 
-			newFollowings = scac.getFollowings(artist)
+			newFollowings = scac.getFollowings(profile)
 			print "New followings: " + ", ".join([scac.id2username(user) if isinstance(scac.id2username(user), str) else str(user) for user in newFollowings])
-			scac.addFollowings(artist, newFollowings, artistGraph)
-			artists_to_enqueue.extend(newFollowings)
+			scac.addFollowings(profile, newFollowings, profileGraph)
+			profiles_to_enqueue.extend(newFollowings)
 
-			newFollowers = scac.getFollowers(artist)
+			newFollowers = scac.getFollowers(profile)
 			print "New followers: " + ", ".join([scac.id2username(user) if isinstance(scac.id2username(user), str) else str(user) for user in newFollowers])
-			scac.addFollowers(artist, newFollowers, artistGraph)
-			artists_to_enqueue.extend(newFollowers)
+			scac.addFollowers(profile, newFollowers, profileGraph)
+			profiles_to_enqueue.extend(newFollowers)
 
-			newFavorites = scac.getFavorites(artist)
+			newFavorites = scac.getFavorites(profile)
 			print "New Favorites: " + ", ".join([scac.id2username(user) if isinstance(scac.id2username(user), str) else str(user) for user in newFavorites])
-			scac.addFavorites(artist, newFavorites, artistGraph)
-			artists_to_enqueue.extend(newFavorites)
+			scac.addFavorites(profile, newFavorites, profileGraph)
+			profiles_to_enqueue.extend(newFavorites)
 
-			newComments = scac.getComments(artist)
+			newComments = scac.getComments(profile)
 			print "New Comments: " + ", ".join([scac.id2username(comment, 'comments') if isinstance(scac.id2username(comment, 'comments'), str) else str(comment) for comment in newComments])
-			scac.addComments(artist, newComments, artistGraph)
-			artists_to_enqueue.extend(newComments)
+			scac.addComments(profile, newComments, profileGraph)
+			profiles_to_enqueue.extend(newComments)
 
-			newTracks = scac.getTracks(artist)
+			newTracks = scac.getTracks(profile)
 			print "New Tracks: " + ", ".join([scac.id2username(track, 'tracks') if isinstance(scac.id2username(track, 'tracks'), str) else str(track) for track in newTracks])
-			scac.addTracks(artist, newTracks, artistGraph)
-			artists_to_enqueue.extend(newTracks)
+			scac.addTracks(profile, newTracks, profileGraph)
+			profiles_to_enqueue.extend(newTracks)
 
 		else:
-			print "\t", "Artist ID %s is not query-able" % artist
-			unavailable_artists.append(artist)
+			print "\t", "Artist ID %s is not query-able" % profile
+			unavailable_profiles.append(profile)
 
-print "The artist graph currently contains " + str(len(artistGraph.nodes())) + " artists."
+print "The profile graph currently contains " + str(len(profileGraph.nodes())) + " profiles."
 
 print "Here are their connections."
 
-print_graph(artistGraph)
+print_graph(profileGraph)
 
-print "The artist graph currently contains " + str(nx.number_strongly_connected_components(artistGraph)) + " strongly connected components."
+print "The profile graph currently contains " + str(nx.number_strongly_connected_components(profileGraph)) + " strongly connected components."
 
 # Go through the graph and compute each PR until it converges.
 iterations = 10
-print "Computing PageRank on our artistGraph..."
-computePR(artistGraph, 0.85, iterations)
+print "Computing PageRank on our profileGraph..."
+computePR(profileGraph, 0.85, iterations)
 
 prList = []
 
-for artist in artistGraph.nodes():
-	prList.append((artist, artistGraph.node[artist]['currPR']))
+for profile in profileGraph.nodes():
+	prList.append((profile, profileGraph.node[profile]['currPR']))
 
 prList.sort(key = lambda tup: tup[1]) # Sort the list in place
 
 prList.reverse() # order by descending PR
 
-print ("Here are some artists similar to " + str(search.username) )
+print ("Here are some profiles similar to " + str(search.username) )
 
 for item in prList[0:10]:
-        artist = scac.id2username(item[0])
-        print artist, item[1]
+        profile = scac.id2username(item[0])
+        print profile, item[1]
