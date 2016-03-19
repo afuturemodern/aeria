1631ea2ecb904c8390f904e818c2f157c84fc62d
Giordon Stark <kratsg@gmail.com>
2016-03-19 18:30:19 -0500

    clean up the sc_api_calls a bit more

--- sc_api_calls.py
+++ sc_api_calls.py
@@ -10,62 +10,49 @@ client = soundcloud.Client(client_id='454aeaee30d3533d6d8f448556b50f23')
 id2username_cache = {}
 artistGraph = Graph()
 
+@handle_http_errors
 def id2username(profile, kind='users'):
 	global id2username_dict
-	try:
-		username = id2username_cache.get(profile, None)
-		while not username:
-			result = client.get('/%s/%s' % (kind, str(profile)))
-                        if kind == 'comments':
-                            username = result.user['username']
-                        elif kind == 'tracks':
-                            username = result.title
-                        else:
-                            username = result.username
-                        # encode it correctly
-                        username = str(username.encode('utf-8'))
-			id2username_cache[profile] = username
-		return username
-	except ConnectionError:
-		print "\t"*2, "id2username(%s): could not make a connection" % profile
-		return None
-	except HTTPError, e:
-		print "\t"*2, "id2username(%s): received an HTTPError" % profile
-                print str(e)
-                return None
-	except UnicodeError:
-		print "\t"*2, "id2username(%s): unicode error in encoding username" % profile
-		return None
+        username = id2username_cache.get(profile, None)
+        if username is not None: return username
+
+        # username is none, we don't have it in cache
+        result = client.get('/%s/%s' % (kind, str(profile)))
+        if kind == 'comments':
+            username = result.user['username']
+        elif kind == 'tracks':
+            username = result.title
+        else:
+            username = result.username
+        # encode it correctly
+        username = str(username.encode('utf-8'))
+        id2username_cache[profile] = username
+        return username
 
 @handle_http_errors
 def getFollowings(profile):
     # get list of users who the artist is following.
-    followings = get_results(client, '/users/{0:s}/followings/'.format(profile))
-    #print "\t", "getFollowings: Analyzing " + id2username(profile) + "\'s " + str(len(followings)) + " followings..."
+    followings = get_results(client, '/users/{0:s}/followings/'.format(str(profile)))
     return followings
 
 @handle_http_errors
 def getFollowers(profile):
     followers = get_results(client, '/users/{0:s}/followers/'.format(str(profile)))
-    #print "\t", "getFollowers: Analyzing " + id2username(profile) + "\'s " + str(len(followers)) + " followers..."
     return followers
 
 @handle_http_errors
 def getFavorites(profile):
-    favorites = get_results('/users/{0:s}/favorites/'.format(str(profile)))
-    #print "\t", "getFavorites: Analyzing " + id2username(profile) + "\'s " + str(len(favorites)) + " favorites..."
+    favorites = get_results(client, '/users/{0:s}/favorites/'.format(str(profile)))
     return favorites
 
 @handle_http_errors
 def getComments(profile):
-    comments = get_results('/users/{0:s}/comments/'.format(str(profile)))
-    #print "\t", "getComments: Analyzing " + id2username(profile)  + "\'s " + str(len(comments)) + " comments..."
+    comments = get_results(client, '/users/{0:s}/comments/'.format(str(profile)))
     return [comment.user['id'] for comment in comments]
 
 @handle_http_errors
 def getTracks(profile):
-    tracks = get_results('/users/{0:s}/tracks/'.format(str(profile)))
-    #print "\t", "getTracks: Analyzing " + id2username(profile) + "\'s " + str(len(tracks)) + " tracks..."
+    tracks = get_results(client, '/users/{0:s}/tracks/'.format(str(profile)))
     return [track.id for track in tracks]
 
 def getWeight(profile, neighbor, artistNet, attr):
@@ -107,13 +94,13 @@ def addComments(artist, comments, artistNet):
 def addTracks(artist, tracks, artistNet):
 	for track in tracks:
 	# get list of users who have favorited this user's track
-		favoriters = get_results('/tracks/' + str(track) + '/favoriters')
+		favoriters = get_results(client, '/tracks/' + str(track) + '/favoriters')
 		print "Adding favoriters for %s" % (id2username(artist))
 		for user in favoriters:
 			addAction(favorites, user.id, artist, addWeight(user.id, artist, artistNet, 'fav_weight'))
 
 	# get list of users who have commented on this user's track
-		commenters = get_results('/tracks/' + str(track) + '/comments')
+		commenters = get_results(client, '/tracks/' + str(track) + '/comments')
 		print "Adding commenters for %s" % (id2username(artist))
 		for comment in commenters:
 			addAction(comments, comment.user['id'], artist, addWeight(comment.user['id'], artist, artistNet, 'com_weight'))
